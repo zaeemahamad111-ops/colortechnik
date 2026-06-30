76a07cdf58535a8020322570223f6ed59f91c218
Muhammed Faris <muhammedfaris710@gmail.com>
2026-06-30 12:26:05 +0530

    feat: replace navbar logo with new transparent version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,20 +50,11 @@ export default function Navbar() {
     >
       <div className="flex justify-between items-center px-grid-margin max-w-[1440px] mx-auto">
         <Link href="/" className="flex items-center z-50">
-          {scrolled ? (
-            <img
-              src="/logo.png"
-              alt="Colortechnik Logo"
-              className="w-32 md:w-44 h-auto object-contain transition-all duration-300"
-            />
-          ) : (
-            <img
-              src="/preloader-logo.png"
-              alt="Colortechnik Logo"
-              className="w-32 md:w-44 h-auto object-contain transition-all duration-300"
-              style={{ mixBlendMode: "screen" }}
-            />
-          )}
+          <img
+            src="/logo-transparent.png"
+            alt="Colortechnik Logo"
+            className="w-32 md:w-44 h-auto object-contain transition-all duration-300"
+          />
         </Link>
         
         {/* Desktop Menu */}
