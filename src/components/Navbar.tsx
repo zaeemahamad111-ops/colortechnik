632923abe85203877f9853a0aeef9654a26f92b5
Muhammed Faris <muhammedfaris710@gmail.com>
2026-06-30 12:16:15 +0530

    feat: replace navbar logo with new CT logo, hide black bg with mix-blend-mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,9 +51,10 @@ export default function Navbar() {
       <div className="flex justify-between items-center px-grid-margin max-w-[1440px] mx-auto">
         <Link href="/" className="flex items-center z-50">
           <img 
-            src="/logo.png"
+            src="/navbar-logo.png"
             alt="Colortechnik Logo" 
             className="w-32 md:w-44 h-auto object-contain transition-opacity duration-300" 
+            style={{ mixBlendMode: "screen" }}
           />
         </Link>
         
